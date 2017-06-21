566cbdb4aa8f1bfb3fdd6e30d5f9bf61bb50a2ec
sarfrazkhan86 <sarfrazkhan86@gmail.com>
2017-06-20 20:46:40 -0700

    Initial commit

---
+++ animations.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(window).on("scroll", function() {
+    $("nav").toggleClass("shrink", $(this).scrollTop() > 50);
+    $("a.brand").toggleClass("shrinkBrand", $(this).scrollTop() > 50);
+  });
+});
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+});
